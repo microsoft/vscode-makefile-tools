dd9b5d72d3ee99e1bab095d9860acccbf68d1fd2
Sneha Ramachandran <snehara@microsoft.com>
2025-03-11 14:41:55 -0400

    updated path

--- src/test/fakeSuite/Repros/.vscode/preconfigure.bat
+++ src/test/fakeSuite/Repros/.vscode/preconfigure.bat
@@ -1,4 +1,4 @@
 @echo off
-set PATH="%cd%\bin\InterestingSmallMakefile\CL\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\12.34.56789\bin\Hostx86\x86";%PATH%
+set PATH="%cd%\bin\InterestingSmallMakefile\CL\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\12.34.56789\bin\Hostx86\x86\%PATH%"
 set PATH=%cd%\bin\GitHub\compilers;%PATH%
 
